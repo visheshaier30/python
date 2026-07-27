ac73b425e533024847bc38a716d11a1ab82dff4a
visheshaier30 <visheshaier30@gmail.com>
2026-07-27 13:19:25 +0530

    Add files via upload

---
+++ breakforloop.py
@@ -0,0 +1,7 @@
+count=0
+while count<=5:
+    if count==3:
+        break
+    print(count)
+    count += 1
+    print("loop treminated")
---
+++ vishesh.py
@@ -0,0 +1,7 @@
+count = 0
+while count < 5:
+    if count == 3:
+        count += 1
+        continue
+    print(count)
+
