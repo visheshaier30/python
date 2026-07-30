ab0e10e115a81bc0ede40572440c91b977bd6704
visheshaier30 <visheshaier30@gmail.com>
2026-07-30 16:07:19 +0530

    Add files via upload

---
+++ valab2.py
@@ -0,0 +1,31 @@
+print("*******************grocery shop details*******************")
+total=0
+items=[3]
+
+n=int(input("enter your items:"))
+name=input("enter your item name :")
+price=float(input("enter your price:"))
+qunity=float(input("enter your qunity:"))
+amount1=price*qunity
+total +=amount1
+
+n=int(input("enter your items:"))
+name=input("enter your item name :")
+price=float(input("enter your price:"))
+qunity=float(input("enter your quaity:"))
+amount2=price*quaity
+total +=amount2
+
+n=int(input("enter your items:"))
+name=input("enter your item name :")
+price=float(input("enter your price:"))
+qunity=float(input("enter your quaity:"))
+amount3=price*quaity
+total +=amount3
+print("*******************bill*******************")
+print("enter items name=",name)
+print("enter price=",price)
+print("enter qunity=",quaity)
+print("*******************total amount*******************")
+print("total amount:",total)
+print("*******************thank you😊*******************")
