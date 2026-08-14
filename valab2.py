8d5b4c1c7733da16e8269fdb43575c0bf3524def
visheshaier30 <visheshaier30@gmail.com>
2026-08-14 20:16:58 +0530

    Update valab2.py

--- valab2.py
+++ valab2.py
@@ -2,7 +2,48 @@ print("*******************grocery shop details*******************")
 total=0
 items=[3]
 
-n=int(input("enter your items:"))
+n=int(input("enter your items:"))print("--------Grocery Shop Detail ------------")
+total=0
+
+n=int(input("Enter your items : "))
+
+
+name =input("Enter your item name :")
+price = float(input("Enter price :"))
+quanity1= float(input("Enter Quanity :"))
+amount1=price*quanity1
+total+=amount1
+
+n=int(input("Enter your items : "))
+
+
+name =input("Enter your item name :")
+price = float(input("Enter price :"))
+quanity2= float(input("Enter Quanity :"))
+amount2=price*quanity2
+total+=amount2
+
+n=int(input("Enter your items : "))
+
+
+name =input("Enter your item name :")
+price = float(input("Enter price :"))
+quanity3= float(input("Enter Quanity :"))
+amount3=price*quanity3
+total+=amount3
+
+price=amount1+amount2+amount3
+quanity=quanity1+quanity2+quanity3
+total=amount1+amount2+amount3
+
+print("--------------- Bill -------------------")
+
+print("Enter price :",price )
+print("Enter quanity :",quanity)
+
+print("---------------TOTAL AMOUNT---------------")
+print("Total Amount :",total)
+print("============THANK YOU =============")
 name=input("enter your item name :")
 price=float(input("enter your price:"))
 qunity=float(input("enter your qunity:"))
