153461e6b1d3af322b3e4e81f0fc00bca1a36010
visheshaier30 <visheshaier30@gmail.com>
2026-08-24 13:41:19 +0530

    Add caste eligibility check for scholarship

--- lab3ex1.py
+++ lab3ex1.py
@@ -1,10 +1,24 @@
+name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 income = float(input("Enter annual family income: "))
+caste = input("Enter your caste (SC/ST/OBC/General): ").upper()
 
 if age < 25:
     if income < 300000:
-        print("Eligible for Specialized Education Scholarship")
+        if caste in ["SC", "ST", "OBC","NT","VJT"]:
+            print("Eligible for Specialized Education Scholarship")
+            result="Eligible for Specialized Education Scholarship"
+        else:
+            print("Not Eligible: Caste is General")
+            result="Not Eligible "
     else:
-        print("Not Eligible: Income is ₹3,00,000 or above")
+        print("Not Eligible: Income is 300000 or above")
 else:
     print("Not Eligible: Age must be below 25")
+
+print("====================== Display ===================")
+print(" Name : ",name)
+print(" Age : ",age)
+print(" Income : ",income)
+print(" Caste : ",caste)
+print("Status",result)
